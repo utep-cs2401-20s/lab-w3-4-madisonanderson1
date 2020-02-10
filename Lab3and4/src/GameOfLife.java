a75d59afff736e02cd1217b9607436af7f72c2be
madia <madisonanderson1@gmail.com>
2020-02-10 13:52:05 -0700

    finish constructors

--- Lab3and4/src/GameOfLife.java
+++ Lab3and4/src/GameOfLife.java
@@ -3,6 +3,14 @@ public class GameOfLife {
     public GameOfLife(){}
 
     public GameOfLife(int size){
-        
+        int[][] previous = new int[size][size]
+        int[][] board = new int[size][size];
     }
+
+    public GameOfLife(int[][] array){
+        int size = array.length;
+        int[][] previous = new int[size][size]
+        int[][] board = new int[size][size];
+    }
+
 }
