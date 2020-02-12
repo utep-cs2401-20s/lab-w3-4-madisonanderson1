1baddad8f784f7a59b3f3ba5ba4a00ccfb799e4e
madia <madisonanderson1@gmail.com>
2020-02-12 14:34:10 -0700

    finish neighbors method

--- Lab3and4/src/GameOfLife.java
+++ Lab3and4/src/GameOfLife.java
@@ -1,22 +1,73 @@
 public class GameOfLife {
 
+    public static void main(String[] args) {
+
+    }
+
+     int size;
+     int[][] previous;
+     int[][] board;
+
     public GameOfLife(){}
 
     public GameOfLife(int size){
-        int[][] previous = new int[size][size]
-        int[][] board = new int[size][size];
+        this.size = size;
+        this.previous = new int[size][size];
+        this.board = new int[size][size];
     }
 
     public GameOfLife(int[][] array){
         int size = array.length;
-        int[][] previous = new int[size][size]
+        int[][] previous = new int[size][size];
         int[][] board = new int[size][size];
 
-        for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array.length; j++){
-                previous[i][j] = array[i][j];
-            }
-        }
+        previous = array;
     }
 
+    public int neighbors (int row, int col){
+        int neighbors = 0;
+        if (row-1 >= 0 && row-1 < board.length) {
+            if (col - 1 >= 0 && col -1 < board.length) {
+                if (board[row - 1][col - 1] == 1) {
+                    neighbors++;
+                }
+            }
+            if (col + 1 >= 0 && col +1 < board.length) {
+                if (board[row - 1][col + 1] == 1) {
+                    neighbors++;
+                }
+            }
+            if (board[row - 1][col] == 1) {
+                neighbors++;
+            }
+        }
+
+        if (col - 1 >= 0 && col-1 < board.length) {
+            if (board[row][col - 1] == 1) {
+                neighbors++;
+            }
+        }
+        if (col+1 >= 0 && col+1 < board.length) {
+            if (board[row][col + 1] == 1) {
+                neighbors++;
+            }
+        }
+        if (row + 1 >= 0 && row+1 < board.length) {
+            if (col - 1 >= 0 && col-1 < board.length) {
+                if (board[row + 1][col - 1] == 1) {
+                    neighbors++;
+                }
+            }
+            if (col+1 >= 0 && col+1 < board.length) {
+                if (board[row + 1][col + 1] == 1) {
+                    neighbors++;
+                }
+            }
+            if (board[row + 1][col] == 1) {
+                neighbors++;
+            }
+        }
+        return neighbors;
+    }
+    
 }
