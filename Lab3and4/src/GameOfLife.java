def1895086694d8b0586503e76a5d1273e60e16d
madia <madisonanderson1@gmail.com>
2020-02-12 15:16:19 -0700

    fix constructor and start one step method

--- Lab3and4/src/GameOfLife.java
+++ Lab3and4/src/GameOfLife.java
@@ -18,10 +18,14 @@ public class GameOfLife {
 
     public GameOfLife(int[][] array){
         int size = array.length;
-        int[][] previous = new int[size][size];
-        int[][] board = new int[size][size];
+        previous = new int[size][size];
+        board = new int[size][size];
 
-        previous = array;
+        for (int i = 0; i < array.length;i++){
+            for (int j = 0; j < array.length; j++){
+                previous[i][j] = array[i][j];
+            }
+        }
     }
 
     public int neighbors (int row, int col){
@@ -68,6 +72,21 @@ public class GameOfLife {
             }
         }
         return neighbors;
+    }
+
+    public void oneStep(){
+        for (int i = 0; i < board.length;i++){
+            for (int j = 0; j < board.length; j++){
+                previous[i][j] = board[i][j];
+            }
+        }
+        for (int i = 0; i< board.length; i++){
+            for (int j = 0; j<board.length;j++){
+                if(neighbors(i,j) < 2){
+                    this.board[i][j] = 0;
+                }
+            }
+        }
     }
     
 }
