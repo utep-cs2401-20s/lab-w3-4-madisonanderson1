781219cce4e7ebd894ffb21513bd94ce1accce0c
madia <madisonanderson1@gmail.com>
2020-02-13 15:21:58 -0700

    one step and evolution

--- Lab3and4/src/GameOfLife.java
+++ Lab3and4/src/GameOfLife.java
@@ -82,10 +82,28 @@ public class GameOfLife {
         }
         for (int i = 0; i< board.length; i++){
             for (int j = 0; j<board.length;j++){
-                if(neighbors(i,j) < 2){
-                    this.board[i][j] = 0;
+                if(board[i][j] == 1){
+                    if(neighbors(i, j) < 2 || neighbors(i, j) > 3){
+                        board[i][j] = 0;
+                    }
+                    if(neighbors(i, j) == 3 || neighbors(i, j) == 2){
+                        board[i][j] = 1;
+                    }
+                }
+                if(board[i][j] == 0){
+                    if(neighbors(i, j) == 3){
+                        board[i][j] = 1;
+                    } else{
+                        board[i][j] = 0;
+                    }
                 }
             }
+        }
+    }
+
+    public void evolution(int n){
+        for(int i = 1; i <= n; i++){
+            oneStep();
         }
     }
     
