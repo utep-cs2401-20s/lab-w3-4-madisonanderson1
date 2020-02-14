461deb2e09d79332657d39e1c6dd2b0d8ea08ce8
madia <madisonanderson1@gmail.com>
2020-02-14 09:29:21 -0700

    override neighbors method in t.g.o.l.

--- Lab3and4/src/TorusGameOfLife.java
+++ Lab3and4/src/TorusGameOfLife.java
@@ -14,6 +14,33 @@ public class TorusGameOfLife extends GameOfLife {
         super(array);
     }
 
-    
-
+    @Override
+    public int neighbors(int row, int col) {
+        int neighbors = 0;
+        if(board[(row-1) % size][col % size] == 1){
+            neighbors++;
+        }
+        if(board[(row-1) % size][(col-1) % size] == 1){
+            neighbors++;
+        }
+        if(board[(row-1) % size][(col+1) % size] == 1){
+            neighbors++;
+        }
+        if(board[row % size][(col-1) % size] == 1){
+            neighbors++;
+        }
+        if(board[row % size][(col+1) % size] == 1){
+            neighbors++;
+        }
+        if(board[(row+1) % size][col % size] == 1){
+            neighbors++;
+        }
+        if(board[(row+1) % size][(col-1) % size] == 1){
+            neighbors++;
+        }
+        if(board[(row+1) % size][(col+1) % size] == 1){
+            neighbors++;
+        }
+       return neighbors;
+    }
 }
